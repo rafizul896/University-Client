a3962342aaaf74ee4a2e0545bb795f36e42cc8c4
rafizul896 <rafizulislam896@gmail.com>
2025-01-22 15:56:29 +0600

    add: Handle verify JWT error and setup a custom base query

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -10,13 +10,17 @@ const baseQuery = fetchBaseQuery({
     if (token) {
       headers.set("authorization", token);
     }
-
-    return headers
+    return headers;
   },
 });
 
+const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+  console.log(result);
+};
+
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery,
+  baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
