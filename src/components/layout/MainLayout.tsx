fa15add650c4c16cfeb2927767004ce0f14ab789
rafizul896 <rafizulislam896@gmail.com>
2025-01-22 13:08:57 +0600

    add: logout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,14 +1,29 @@
 import { Layout } from "antd";
-const { Header, Content, Footer } = Layout;
+const { Header, Content } = Layout;
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import { useAppDispatch } from "@/redux/hooks";
+import { logout } from "@/redux/features/auth/authSlice";
+import { MdLogout } from "react-icons/md";
 
 const MainLayout = () => {
+  const dispatch = useAppDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
   return (
     <Layout style={{ height: "100vh" }}>
       <Sidebar />
       <Layout>
-        <Header style={{ padding: 0 }} />
+        <Header style={{ padding: 0 }}>
+          <button
+            onClick={handleLogout}
+            className="text-red-500 flex items-center gap-1 absolute right-5"
+          >
+            <MdLogout /> Logout
+          </button>
+        </Header>
         <Content style={{ margin: "24px 16px 0" }}>
           <div
             style={{
@@ -19,9 +34,6 @@ const MainLayout = () => {
             <Outlet />
           </div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Footer>
       </Layout>
     </Layout>
   );
