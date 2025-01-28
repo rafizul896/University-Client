63253218cd2209a8219f63710076c862811d9d56
rafizul896 <rafizulislam896@gmail.com>
2025-01-28 18:53:56 +0600

    add: academic management routes

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -3,6 +3,7 @@ import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
+import CreateAcademicSemester from "@/pages/admin/academicManagement/CreateAcademicSemester";
 
 export const adminPaths = [
   {
@@ -14,9 +15,34 @@ export const adminPaths = [
     name: "Academic Management",
     children: [
       {
+        name: "Create A. Semester",
+        path: "create-academic-semester",
+        element: <CreateAcademicSemester />,
+      },
+      {
         name: "Academic Semester",
         path: "academic-semester",
         element: <AcademicSemester />,
+      },
+      {
+        name: "Create A. Faculty",
+        path: "create-academic-faculty",
+        element: <CreateAcademicSemester />,
+      },
+      {
+        name: "Academic Faculty",
+        path: "academic-faculty",
+        element: <CreateAcademicSemester />,
+      },
+      {
+        name: "Create A. Department",
+        path: "create-academic-department",
+        element: <CreateAcademicSemester />,
+      },
+      {
+        name: "Academic Department",
+        path: "academic-department",
+        element: <CreateAcademicSemester />,
       },
     ],
   },
