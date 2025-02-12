5b223bce42c2d77f17f8dd6f12932f24b9781a50
rafizul896 <rafizulislam896@gmail.com>
2025-02-12 17:45:22 +0600

    add: ceate student page is complete

--- src/components/form/PHSelect.tsx
+++ src/components/form/PHSelect.tsx
@@ -4,16 +4,17 @@ import { Controller } from "react-hook-form";
 type TSelectProps = {
   name: string;
   label?: string;
-  options: { value: string; label: string; disabled?: boolean }[];
+  options: { value: string; label: string; disabled?: boolean }[] | undefined;
+  disabled?: boolean;
 };
 
-const PHSelect = ({ name, label, options }: TSelectProps) => {
+const PHSelect = ({ name, label, options, disabled }: TSelectProps) => {
   return (
     <Controller
       name={name}
       render={({ field, fieldState: { error } }) => (
         <Form.Item label={label}>
-          <Select {...field} options={options} />
+          <Select {...field} options={options} disabled={disabled} />
           {error && <small className="text-rose-500">{error?.message}</small>}
         </Form.Item>
       )}
--- src/pages/admin/userManagement/CreateStudent.tsx
+++ src/pages/admin/userManagement/CreateStudent.tsx
@@ -3,22 +3,94 @@ import PHForm from "@/components/form/PHForm";
 import PHInput from "@/components/form/PHInput";
 import PHSelect from "@/components/form/PHSelect";
 import { bloodGroupOptions, genderOptions } from "@/constants/global";
-import { Button, Col, Divider, Row } from "antd";
-import { FieldValues, SubmitErrorHandler } from "react-hook-form";
+import {
+  useGetAcademicDepartmentsQuery,
+  useGetAllSemestersQuery,
+} from "@/redux/features/admin/academicManagement.api";
+import { useAddAStudentMutation } from "@/redux/features/admin/userManagement.api";
+import { Button, Col, Divider, Form, Input, Row } from "antd";
+import { Controller, FieldValues } from "react-hook-form";
+import { toast } from "sonner";
+
+const defaultValue = {
+  name: {
+    firstName: "I am ",
+    middleName: "Student",
+    lastName: "Number 1",
+  },
+  gender: "male",
+  email: "student2@gmail.com",
+  contactNo: "1235678",
+  emergencyContactNo: "987-654-3210",
+  bloodGroup: "A+",
+  presentAddress: "123 Main St, Cityville",
+  permanentAddress: "456 Oak St, Townsville",
+  guardian: {
+    fatherName: "James Doe",
+    fatherOccupation: "Engineer",
+    fatherContactNo: "111-222-3333",
+    motherName: "Mary Doe",
+    motherOccupation: "Teacher",
+    motherContactNo: "444-555-6666",
+  },
+  localGuardian: {
+    name: "Alice Johnson",
+    occupation: "Doctor",
+    contactNo: "777-888-9999",
+    address: "789 Pine St, Villageton",
+  },
+};
 
 const CreateStudent = () => {
-  const onSubmit: SubmitErrorHandler<FieldValues> = (data) => {
-    console.log(data);
+  const [createStudent] = useAddAStudentMutation();
+  const { data: semesterData, isLoading: sIsLoading } =
+    useGetAllSemestersQuery(undefined);
+  const { data: departmentData, isLoading: dIsLoading } =
+    useGetAcademicDepartmentsQuery(undefined, { skip: sIsLoading });
 
-    // const formData = new FormData();
-    // formData.append("data", JSON.stringify(data));
+  const semesterOptions = semesterData?.data?.map((item) => ({
+    value: item._id,
+    label: `${item.name}-${item.year}`,
+  }));
+
+  const departmentOptions = departmentData?.data?.map((item) => ({
+    value: item._id,
+    label: item.name,
+  }));
+
+  const onSubmit = async (data: FieldValues) => {
+    const toastId = toast.loading("creating..!");
+    const formData = new FormData();
+
+    const studentData = {
+      password: "student123",
+      student: data,
+    };
+
+    formData.append("data", JSON.stringify(studentData));
+    formData.append("file", data.image);
+
     //! THis is for development --> just for checking
     // console.log(Object.fromEntries(formData));
+
+    try {
+      const res = await createStudent(formData);
+      if (res.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      } else {
+        toast.success("Student is Created successfully", { id: toastId });
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        toast.error(err?.message, { id: toastId });
+      }
+    }
   };
+
   return (
     <Row>
       <Col span={24}>
-        <PHForm onSubmit={onSubmit}>
+        <PHForm onSubmit={onSubmit} defaultValues={defaultValue}>
           <Divider>Personal Information</Divider>
           <Row gutter={8}>
             <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
@@ -34,13 +106,28 @@ const CreateStudent = () => {
               <PHSelect options={genderOptions} name="gender" label="Gender" />
             </Col>
             <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
-              <PHDatePicker  name="dateOfBirth" label="Date Of Birth" />
+              <PHDatePicker name="dateOfBirth" label="Date Of Birth" />
             </Col>
             <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
               <PHSelect
                 options={bloodGroupOptions}
                 name="bloodGroup"
                 label="Blood Group"
+              />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <Controller
+                name="image"
+                render={({ field: { onChange, value, ...field } }) => (
+                  <Form.Item label="Picture">
+                    <Input
+                      type="file"
+                      value={value?.fileName}
+                      {...field}
+                      onChange={(e) => onChange(e.target.files?.[0])}
+                    />
+                  </Form.Item>
+                )}
               />
             </Col>
           </Row>
@@ -137,7 +224,7 @@ const CreateStudent = () => {
             </Col>
             <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
               <PHInput
-                type="number"
+                type="text"
                 name="localGuardian.contactNo"
                 label="Contact Number"
               />
@@ -154,15 +241,17 @@ const CreateStudent = () => {
           <Divider>Academic Information</Divider>
           <Row gutter={8}>
             <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
-              <PHInput
-                type="text"
+              <PHSelect
+                options={departmentOptions}
+                disabled={dIsLoading}
                 name="academicDepartment"
                 label="Academic Department"
               />
             </Col>
             <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
-              <PHInput
-                type="text"
+              <PHSelect
+                options={semesterOptions}
+                disabled={sIsLoading}
                 name="admissionSemester"
                 label="Admission Semester"
               />
