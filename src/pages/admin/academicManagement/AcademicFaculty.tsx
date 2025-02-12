76b40b699cb61e8221fd5ac3802b1d2792095634
rafizul896 <rafizulislam896@gmail.com>
2025-02-12 08:58:41 +0600

    add: academicFaculty page

--- src/pages/admin/academicManagement/AcademicFaculty.tsx
+++ src/pages/admin/academicManagement/AcademicFaculty.tsx
@@ -1,10 +1,41 @@
 import { useGetAcademicFacultyQuery } from "@/redux/features/admin/academicManagement.api";
+import { Button, Table, TableColumnsType } from "antd";
+
+export type TTableData = { name: string };
 
 const AcademicFaculty = () => {
-    const {data} = useGetAcademicFacultyQuery(undefined);
+  const { data, isFetching } = useGetAcademicFacultyQuery(undefined);
 
-    console.log(data)
-  return <div>AcademicFaculty</div>;
+  const tabelData = data?.data?.map(({ _id, name }) => ({
+    key: _id,
+    name,
+  }));
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: "Name",
+      key: "name",
+      dataIndex: "name",
+    },
+    {
+      title: "Action",
+      key: "x",
+      render: () => {
+        return (
+          <div>
+            <Button>Update</Button>
+          </div>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Table<TTableData>
+      loading={isFetching}
+      columns={columns}
+      dataSource={tabelData}
+    />
+  );
 };
 
 export default AcademicFaculty;
